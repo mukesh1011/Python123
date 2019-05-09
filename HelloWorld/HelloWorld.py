512cd67f126b5d6da6ecbdafd90ab54120fafd34
Mukesh <prajapati0810@gmail.com>
2019-05-09 22:38:46 +0100

    commit 10052019

---
+++ HelloWorld/HelloWorld.py
@@ -0,0 +1,14 @@
+print('Hello, World')
+print(1 + 2)
+print(7 * 6)
+print()
+print("The End")
+print("Python's strings are easy to use")
+print('We can eve include "Quotes" in strings')
+print("Hello" + " world")
+greeting = "Hello"
+name = "Mukesh"
+print(greeting + name)
+#comment
+print(greeting + ' ' + name)
+
