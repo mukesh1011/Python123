e21d81f18e2493e4094d8d37ad32b20229f5441e
Mukesh <prajapati0810@gmail.com>
2020-03-26 09:49:06 +0000

    commit 26032020

---
+++ Selenuim101/test1.py
@@ -0,0 +1,13 @@
+from selenium import webdriver
+
+driver = webdriver.Chrome()
+driver.get('https://youtube.com')
+
+searchbox = driver.find_element_by_xpath('//*[@id="search"]')
+searchbox.send_keys('Hitesh Choudhary')
+
+searchbutton = driver.find_element_by_xpath('//*[@id="search-icon-legacy"]/yt-icon')
+searchbutton.click()
+
+
+
